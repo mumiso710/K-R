f2124bdd9f8e53b18babbf5d94797236b31a33e9
mumiso <zkayrodu@gmail.com>
2023-12-02 01:11:47 +0900

    complete 1-16 1-17 1-18 1-19

---
+++ 1/1-16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int get_line(char line[], int maxline);
+
+int main() {
+    int len;
+    char line[MAXLINE];
+
+    while ((len = get_line(line, MAXLINE)) > 0) {
+        printf("line length: %d\n", len);
+
+        if (len > 0) {
+            printf("line: %s\n", line);
+        }
+    }
+    return 0;
+}
+
+int get_line(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+        if (c == '\n') {
+            s[i] = c;
+            ++i;
+        }
+    }
+    s[i] = '\0';
+    return i;
+}
---
+++ 1/1-18.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int get_line_without_white_space(char line[], int maxline);
+
+int main() {
+    int len;
+    char line[MAXLINE];
+
+    while ((len = get_line_without_white_space(line, MAXLINE)) > 0) {
+        printf("line length: %d\n", len);
+
+        if (len > 0) {
+            printf("line: %s", line);
+        }
+    }
+    return 0;
+}
+int get_line_without_white_space(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        if (c != ' ' && c != '\t') {
+            s[i] = c;
+        }
+
+        if (c == ' ' || c == '\t') {
+            --i;
+        }
+
+        if (c == '\n') {
+            ++i;
+        }
+    }
+    s[i] = '\0';
+    return i;
+}
