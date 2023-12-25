ecee1e393176bf2a6259f0387469fb2107ed457d
mumiso710 <zkayrodu@gmail.com>
2023-12-25 16:24:48 +0900

    complete 2-2

---
+++ 2/2-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int lim;
+    int c;
+
+    for (i = 0; i < lim - 1; ++i) {
+        c = getchar();
+        if (c == '\n') {
+            break;
+        }
+
+        if (c == EOF) {
+            break;
+        }
+    }
+}
