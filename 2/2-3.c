52a7a86e6fd37e1d04401e467dad0f37fe81acfc
mumiso <zkayrodu@gmail.com>
2023-12-25 20:33:08 +0900

    complete 2-3.c

---
+++ 2/2-3.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int htoi(char[]);
+
+int main() {
+    char hex[] = "0x3A";
+    int converted = htoi(hex);
+    printf("%d\n", converted);
+}
+
+int htoi(char s[]) {
+    int len = strlen(s);
+    int num = 0;
+    int counter = 0;
+    for (int i = len - 1; i >= 0; i--) {
+        if (s[i] == 'x' || s[i] == 'X') {
+            break;
+        }
+
+        if (s[i] >= 'A' && s[i] <= 'F') {
+            num += pow(16, counter) * (s[i] - 'A' + 10);
+        } else {
+            num += pow(16, counter) * (s[i] - '0');
+        }
+        counter += 1;
+    }
+    return num;
+}
---
+++ 2/2-4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+void squeeze(char target[], char patterns[]);
+
+int main() {
+    char s1[] = "aiueo";
+    char s2[] = "o";
+    printf("before squeeze: %s\n", s1);
+    squeeze(s1, s2);
+    printf("patterns: %s\n", s2);
+    printf("after squeeze: %s\n", s1);
+}
+
+void squeeze(char target[], char patterns[]) {
+    int i, j, k;
+    i = k = 0;
+
+    int len = strlen(target);
+    char temp_target[len + 1];
+    strcpy(temp_target, target);
+
+    while (temp_target[i] != '\0') {
+        int in_patterns = 0;
+        j = 0;
+        while (patterns[j] != '\0') {
+            if (temp_target[i] == patterns[j++]) {
+                in_patterns = 1;
+            }
+        }
+        if (!in_patterns) {
+            target[k++] = temp_target[i];
+        }
+        i += 1;
+    }
+    target[k] = '\0';
+}
