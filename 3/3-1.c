75f2974c4ab5aee9304f4f51ce2fab5444adfdf2
mumiso <zkayrodu@outlook.jp>
2024-04-28 01:57:51 +0900

    complete 3-1

---
+++ 3/3-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int binsearch(int x, int v[], int n) {
+  int low, high, mid;
+
+  low = 0;
+  high = n - 1;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (x <= v[mid]) {
+      high = mid;
+    } else if (x > v[mid]) {
+      low = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return (x == v[low]) ? low : -1;
+}
